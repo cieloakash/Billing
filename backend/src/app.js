14d9b509fa4ef0fbccdb429cc8b1d981b2b6738e
Sky <akpakash490@gmail.com>
2025-05-21 21:03:02 +0530

    fix prod

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(cors({
   origin: process.env.ORIGIN || "http://localhost:5173",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"]
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(cookieParser())
 
